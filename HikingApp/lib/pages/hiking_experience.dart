75018acb517e8da035d90da2573d1c4b4f695568
Analeigh Howard <ahoward815@byu.edu>
2022-03-25 15:46:19 -0600

    radio button list partially done for first welcome page

---
+++ HikingApp/lib/pages/hiking_experience.dart
@@ -0,0 +1,86 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RateYourExperience extends StatefulWidget {
+  @override
+  _RateYourExperience createState() => _RateYourExperience();
+}
+
+enum RadioButton { one, two, three, four, five }
+
+class _RateYourExperience extends State<RateYourExperience> {
+  RadioButton? _button = RadioButton.one;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent[200],
+        title: Text('HikR'),
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          RadioListTile(
+            title: const Text('4+ times per week'),
+            value: RadioButton.one,
+            groupValue: _button,
+            onChanged: (RadioButton? value) {
+              setState(() {
+                _button = value;
+              });
+            },
+          ),
+          RadioListTile(
+            title: const Text('1-3 times per week'),
+            value: RadioButton.two,
+            groupValue: _button,
+            onChanged: (RadioButton? value) {
+              setState(() {
+                _button = value;
+              });
+            },
+          ),
+          RadioListTile(
+            title: const Text('1-3 times per month'),
+            value: RadioButton.three,
+            groupValue: _button,
+            onChanged: (RadioButton? value) {
+              setState(() {
+                _button = value;
+              });
+            },
+          ),
+          RadioListTile(
+            title: const Text('1-3 times per year'),
+            value: RadioButton.four,
+            groupValue: _button,
+            onChanged: (RadioButton? value) {
+              setState(() {
+                _button = value;
+              });
+            },
+          ),
+          RadioListTile(
+            title: const Text('Never'),
+            value: RadioButton.five,
+            groupValue: _button,
+            onChanged: (RadioButton? value) {
+              setState(() {
+                _button = value;
+              });
+            },
+          ),
+        ],
+      )
+    );
+  }
+
+}

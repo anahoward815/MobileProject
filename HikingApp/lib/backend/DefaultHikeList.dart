3fe6b69f1b2c039c79f8c0df0c7ef4f05a437e53
ebarton2 <70668709+ebarton2@users.noreply.github.com>
2022-04-09 17:19:41 -0600

    Added map image functionality

--- HikingApp/lib/backend/DefaultHikeList.dart
+++ HikingApp/lib/backend/DefaultHikeList.dart
@@ -2,11 +2,24 @@ import 'Hikes.dart';
 
 class DefaultHikeList {
   final List<Hikes> _hikes = [
-    Hikes("Timpanogos", "Hard", 'Mountain/Rock', 'Utah County', 'assets/images/timpanogos.jpg', 4400.0, 13.0, 40.40731, -111.65467),
-    Hikes("Bridal Veil Trail", 'Easy', 'Paved', 'Utah County', 'assets/images/bridal.jpg', 114.0, 1.4, 40.33666532, -111.60083093),
+    Hikes("Timpanogos", "Hard", 'Mountain/Rock', 'Utah County', 'assets/images/timpanogos.jpg', 'assets/images/map_images/timp_trail.jpg', 4400.0, 13.0, 40.40731, -111.65467),
+    Hikes("Bridal Veil Trail", 'Easy', 'Paved', 'Utah County', 'assets/images/bridal.jpg', 'assets/images/map_images/timp_trail.jpg', 114.0, 1.4, 40.33666532, -111.60083093),
+    Hikes("Rock Garden Trail", 'Intermediate', 'Gravel', 'Utah County', 'assets/images/rockgarden.jpg', 'assets/images/map_images/timp_trail.jpg', 1555.0, 3.0, 40.33666532, -111.60083093),
+    Hikes("Squaw Peak Trail", 'Hard', 'Mountain/Rock', 'Utah County', 'assets/images/squaw.jpg', 'assets/images/map_images/timp_trail.jpg', 2831.0, 7.8, 40.33666532, -111.60083093),
+    Hikes("Boulder Mail Trail", 'Hard', 'Rocky', 'Escalante National Monument', 'assets/images/boulder.jpg', 'assets/images/map_images/timp_trail.jpg', 2582.0, 15.4, 40.33666532, -111.60083093),
+    Hikes("Utah Lake Shoreline", 'Easy', 'Paved', 'Utah County', 'assets/images/shoreline.jpg', 'assets/images/map_images/timp_trail.jpg', 26.0, 4.1, 40.33666532, -111.60083093),
+    Hikes("Copper Pit Overlook", 'Intermediate', 'Gravel', 'Tooele County', 'assets/images/copper.jpg', 'assets/images/map_images/timp_trail.jpg', 1243.0, 5.1, 40.33666532, -111.60083093),
   ];
 
   List<Hikes> getHikeList() {
     return _hikes;
   }
+/*
+'assets/images/map_images/timp_trail.jpg',
+  Hikes("Rock Garden Trail", 'Intermediate', 'Gravel', 'Utah County', 'assets/images/rockgarden.jpg', 1555.0, 3.0),
+  Hikes("Squaw Peak Trail", 'Hard', 'Mountain/Rock', 'Utah County', 'assets/images/squaw.jpg', 2831.0, 7.8),
+  Hikes("Boulder Mail Trail", 'Hard', 'Rocky', 'Escalante National Monument', 'assets/images/boulder.jpg', 2582.0, 15.4),
+  Hikes("Utah Lake Shoreline", 'Easy', 'Paved', 'Utah County', 'assets/images/shoreline.jpg', 26.0, 4.1),
+  Hikes("Copper Pit Overlook", 'Intermediate', 'Gravel', 'Tooele County', 'assets/images/copper.jpg', 1243.0, 5.1),
+ */
 }
--- HikingApp/lib/backend/Hikes.dart
+++ HikingApp/lib/backend/Hikes.dart
@@ -7,12 +7,13 @@ class Hikes {
   late String hikeTerrain;
   late String hikeLocation;
   late String hikeImagePath;
+  late String hikeMapPath;
   late double hikeElevation;
   late double hikeLength;
   late double lat;
   late double long;
 
-  Hikes(String name, String difficulty, String terrain, String location, String imagePath, double elevation, double length, this.lat,this.long) {
+  Hikes(String name, String difficulty, String terrain, String location, String imagePath, String mapPath, double elevation, double length, this.lat,this.long) {
     hikeName = name;
     hikeDifficulty = difficulty;
     hikeTerrain = terrain;
@@ -36,6 +37,10 @@ class Hikes {
 
   String getHikeImagePath() {
     return hikeImagePath;
+  }
+
+  String getHikeMapPath() {
+    return hikeMapPath;
   }
 
   String getHikeLocation() {

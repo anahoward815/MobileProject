aad5f334c4cc942de6db3086d9a3714e6540da67
Chris Mero <chris.mero97@gmail.com>
2022-04-11 09:56:27 -0600

    changed name to something better

--- HikingApp/lib/reusable_widgets/AppHeader.dart
+++ HikingApp/lib/reusable_widgets/AppHeader.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 PreferredSizeWidget appHeader() {
   return AppBar(
     backgroundColor: Colors.orangeAccent[100],
-    title: Text('HikR'),
+    title: Text('My Hikes'),
 
   );
 }

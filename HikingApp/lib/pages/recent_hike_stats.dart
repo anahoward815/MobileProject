d09e7aefbe8ba6d5655f10cf8f05bb6dc39a3a4d
Chris Mero <chris.mero97@gmail.com>
2022-04-12 16:28:18 -0600

    imporved the stats page by dividing the stats into sub-groups and putting each sub-group into a card

--- HikingApp/lib/pages/recent_hike_stats.dart
+++ HikingApp/lib/pages/recent_hike_stats.dart
@@ -29,105 +29,184 @@ class _RecentHikeStatsState extends State<RecentHikeStats> {
 
     return Scaffold(
       appBar: appHeader(),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image(
-              image: AssetImage(recentHike.getHikeImagePath()),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '${recentHike.getHikeName()}',
-              style: TextStyle(
-                fontSize: 30.0,
-
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '${recentHike.getHikeName()}',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 50.0,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Card(
-              child: Column(
+              Image(
+                image: AssetImage(recentHike.getHikeImagePath()),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   Text(
                     'Stats:',
                     style: TextStyle(
-                      fontSize: 20.0,
+                      fontSize: 40.0,
                     ),
                   ),
                   SizedBox(height: 5.0),
-                  Text(
-                    'Total Time: 12 hrs 45 mins',
-                    style: TextStyle(
-                      fontSize: 15.0,
+                  Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(5.0, 5.0, 0.0, 0.0),
+                          child: Text(
+                            'Distance and Time:',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 0.0),
+                          child: Text(
+                            'Total Time: 12 hrs 45 mins',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 0.0),
+                          child: Text(
+                            'Total Distance: 14.60 miles',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 0.0),
+                          child: Text(
+                            'Elevation Gain: 5,557’',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Calories Burned: 4428 cal',
-                    style: TextStyle(
-                      fontSize: 15.0,
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(5.0, 5.0, 0.0, 0.0),
+                          child: Text(
+                            'Caloric Expenditure:',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 0.0),
+                          child: Text(
+                            'Calories Burned: 4428 cal',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Total Distance: 14.60 miles',
-                    style: TextStyle(
-                      fontSize: 15.0,
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(5.0, 5.0, 0.0, 0.0),
+                          child: Text(
+                            'Breaks, Averages, and Personal Bests :',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 0.0),
+                          child: Text(
+                            'Breaks Taken: 10',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 0.0),
+                          child: Text(
+                            'Average Break: 5 mins',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 0.0),
+                          child: Text(
+                            'Average mph: 3.0 mph',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 0.0),
+                          child: Text(
+                            'Longest Non-Stop Distance: 1.64 miles',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 0.0),
+                          child: Text(
+                            'Greatest Elevation Gain: 500’',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Elevation Gain: 5,557’',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Breaks Taken: 10',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Average Break: 5 mins',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Average mph: 3.0 mph',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Longest Non-Stop Distance: 1.64 miles',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Greatest Elevation Gain: 500’',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  SizedBox(height: 5.0),
                 ],
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
       drawer: hamburgerDrawer(context),
